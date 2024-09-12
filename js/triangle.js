5d2a9efb1564d8c677b6b23c37ccecea61d6c11a
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-09-12 21:27:51 +0600

    get base and height value and convert to number

--- js/triangle.js
+++ js/triangle.js
@@ -1,3 +1,10 @@
 function calculateTriangleArea(){
+    // Get base value
+    const triangleBaseInput = document.getElementById("triangle-base").value ; // string value
+    const base = parseFloat(triangleBaseInput); //convert to number
+
+    // Get height value
+    const triangleHeightInput = document.getElementById("triangle-height").value ; // string value
+    const height = parseFloat(triangleHeightInput); //convert to number
     
 }
