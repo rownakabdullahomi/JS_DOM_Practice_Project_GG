d53911754527aa8d48daa1539c3288854315686d
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-09-14 01:32:14 +0600

    calculate rectangle area using addEventListener() function

--- js/all-calculation.js
+++ js/all-calculation.js
@@ -1,33 +1,58 @@
-function calculateTriangleArea(){
+function calculateTriangleArea() {
     const base = getFirstValue("triangle-base");
     const height = getSecondValue("triangle-height");
     const triangleArea = 0.5 * base * height;
-    console.log(triangleArea);
-    
+    //console.log(triangleArea);
+
     showArea("triangle-area", triangleArea);
-    showAreaTextBox("triangle-area-input", triangleArea)
-    
+    showAreaTextBox("triangle-area-input", triangleArea);
 }
 
-function getFirstValue(firstInputId){
+//Rectangle Area Calculate Using addEventListener() function instead of onclick() function
+//------------------  Method 1 (Preferable) Using Anonymous Function ---------------------
+const btnRectangleCalculate = document.getElementById("btn-rectangle-calculate");
+btnRectangleCalculate.addEventListener("click", function () {
+    const width = getFirstValue("rectangle-width");
+    const length = getSecondValue("rectangle-length");
+    const rectangleArea = width * length;
+
+    showArea("rectangle-area", rectangleArea);
+    showAreaTextBox("rectangle-area-input", rectangleArea);
+});
+
+//--------------------  Method 2 Using Defined Function ------------------------------
+// const btnRectangleCalculate = document.getElementById("btn-rectangle-calculate");
+// btnRectangleCalculate.addEventListener("click", calculateRectangleArea());
+// function calculateRectangleArea() {
+//     const width = getFirstValue("rectangle-width");
+//     const length = getSecondValue("rectangle-length");
+//     const rectangleArea = width * length;
+
+//     showArea("rectangle-area", rectangleArea);
+//     showAreaTextBox("rectangle-area-input", rectangleArea);
+// }
+
+
+
+function getFirstValue(firstInputId) {
     const firstText = document.getElementById(firstInputId).value;
     const firstValue = parseFloat(firstText);
     return firstValue;
 }
-function getSecondValue(secondInputId){
+function getSecondValue(secondInputId) {
     const secondText = document.getElementById(secondInputId).value;
     const secondValue = parseFloat(secondText);
     return secondValue;
 }
 
-function showArea(placeId, area){
+function showArea(placeId, area) {
     const place = document.getElementById(placeId);
     place.innerText = area;
 }
-function showAreaTextBox(textBoxId, area){
+function showAreaTextBox(textBoxId, area) {
     const textBox = document.getElementById(textBoxId);
     //textBox.style.display = "block";   it is also ok
     textBox.removeAttribute("hidden");
     textBox.style.textAlign = "center";
-    textBox.value = area
+    textBox.value = area;
 }
